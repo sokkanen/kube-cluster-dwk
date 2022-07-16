1c53296a88daffe5a198ab13188b1b92e26a6d2c
sokkanen <joel.sokkanen@helsinki.fi>
2022-07-16 15:44:21 +0300

    Yet again

--- create-and-bootstrap-cluster.sh
+++ create-and-bootstrap-cluster.sh
@@ -4,8 +4,8 @@ k3d cluster create --port 8082:30080@agent:0 -p 8081:80@loadbalancer --agents 2
 
 kubectl create namespace flux-system
 
-kubectl -n flux-system create secret generic sops-age \
-  --from-file=sops.age=./key.txt
+kubectl -n flux-system create secret generic sops-gpg \
+  --from-file=sops.asc=./key.txt
 
 flux check --pre
 
