9f01aab812763d0a1db9916cbcb528937a3b4a86
sokkanen <joel.sokkanen@helsinki.fi>
2022-07-16 14:44:23 +0300

    Relocating the secret encryption

--- create-and-bootstrap-cluster.sh
+++ create-and-bootstrap-cluster.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
 k3d cluster create --port 8082:30080@agent:0 -p 8081:80@loadbalancer --agents 2
+
+flux check --pre
+
+flux bootstrap github \
+    --owner=sokkanen \
+    --repository=kube-cluster-dwk \
+    --personal \
+    --private=false
+
+kubectl -n flux-system create secret generic sops-age \
+  --from-file=sops.age=./key.txt
